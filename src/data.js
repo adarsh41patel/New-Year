c079e78e876120ba1f8e1a4004f162b90f06d405
adarsh <adarsh@gmail.com>
2025-12-31 00:41:54 +0530

    Initial project commit

---
+++ src/data.js
@@ -0,0 +1,98 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const introHeading = "A little surprise for you";
+// Intro Heading - Change this text as you like
+
+export const introSubHeading = "Before this year begins";
+// Intro Subheading - You can edit freely
+
+// ==========================
+// 2. Gift Screen
+// ==========================
+export const giftHeading = `Something is waiting for you`;
+// Gift Heading - You can edit freely
+
+export const giftSubHeading = `Move the little distractions aside`;
+// Subheading below Gift Heading - You can edit freely
+
+export const ticketText = `Unlimited Love & Smiles`;
+// Text inside the ticket - You can change it to 'Unlimited Hugs', 'Endless Joy', or anything you like
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Some moments from this year";
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.webp" }, Match the format and add more as needed
+export const photos = [
+  "/images/1.png",
+  "/images/2.png",
+  "/images/3.png",
+  "/images/4.png",
+  "/images/5.png",
+  "/images/6.png",
+  "/images/7.png",
+  "/images/8.png",
+  "/images/9.png",
+  "/images/10.png",
+  "/images/11.png",
+];
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "For my Cutiepie :)";
+// Message Heading - Change this text as you like
+
+export const specialMessage = ` 
+May this year fill your life with all the happiness
+and dreams your heart has been holding onto.
+May you receive the love you deserve,
+the dreams you’ve wished for,
+and the peace you’ve been searching for 🤍
+
+This year may shape you, teach you,
+and gift you countless beautiful memories.
+There may be ups and downs,
+but as you step into the New Year,
+may it welcome you with new hopes,
+new joys, and brighter days ahead 🌱
+
+You truly deserve all the happiness coming your way 
+so open your arms, embrace what life gives you,
+and be grateful for every blessing you already have ✨
+
+I made a list of special people in my life with a pencil,
+but when I reached your name, I used a pen 
+because I’m sure I’ll never have to make any changes.
+I love you more than words can express,
+and I will always love you ❤️
+
+I hope this year brings you calm mornings,
+peaceful nights,
+and little moments that make you smile
+for no reason at all 🌙
+
+No matter what this year holds,
+I hope you always feel supported, valued,
+and deeply understood.
+May every challenge turn into strength,
+and every quiet moment remind you
+of how special you truly are 🌸
+
+You deserve a year filled with warmth, meaning,
+and gentle happiness.
+May this year be kind to you
+and give you everything your heart has been hoping for ✨
+
+🌟 Wishing you a beautiful New Year Rajvi🌟`;
+// Your message (you can edit freely)
+
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3";
